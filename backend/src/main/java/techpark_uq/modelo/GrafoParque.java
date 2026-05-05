8d4540b06fc0f25e76be8de47d5933ae84172290
SaraaB10 <sarasofiabolanospelayo@gmail.com>
2026-05-05 18:39:19 -0500

    feat: Implementación de rutaOptima()
    Implementar método de rutaOptima() con Dijkstra en GrafoParque

--- backend/src/main/java/techpark_uq/modelo/GrafoParque.java
+++ backend/src/main/java/techpark_uq/modelo/GrafoParque.java
@@ -22,4 +22,16 @@ public class GrafoParque {
     public void agregarSendero(String idOrigen, String idDestino, double distancia) {
         grafo.agregarArista(idOrigen, idDestino, distancia);
     }
+
+    // Ruta optima con Dijkstra
+    public List<Atraccion> rutaOptima(String idOrigen, String idDestino) {
+        List<String> ids = grafo.rutaOptima(idOrigen, idDestino);
+        List<Atraccion> ruta = new ArrayList<>();
+        for (String id : ids) {
+            Atraccion a = nodos.get(id);
+            if (a != null) ruta.add(a);
+        }
+        return ruta;
+    }
+
 }
